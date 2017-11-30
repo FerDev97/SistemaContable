dd282e096ff9e756da21fc3651e22b0db0d924c2
kornflakesguy <fer.aravalo1997@gmail.com>
2017-11-29 18:30:41 -0600

    1.0

---
+++ pages/menu.php
@@ -0,0 +1,28 @@
+<!-- start:Left Menu -->
+            <div id="left-menu">
+              <div class="sub-left-menu scroll">
+                <ul class="nav nav-list">
+                    <li><div class="left-bg"></div></li>
+                    <li class="time">
+                      <h1 class="animated fadeInLeft">21:00</h1>
+                      <p class="animated fadeInRight">Sat,October 1st 2029</p>
+                    </li>
+                    <li class="active ripple">
+                      <a class="tree-toggle nav-header"><span class="fa-home fa"></span> Catalogo
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                      <ul class="nav nav-list tree">
+
+                          <li><a href="cuenta.php">Mantenimiento</a></li>
+                      </ul>
+                      <ul class="nav nav-list tree">
+
+                          <li><a href="listacuenta.php">Lista</a></li>
+                      </ul>
+                    </li>
+
+
+                  </ul>
+                </div>
+            </div>
+          <!-- end: Left Menu -->
