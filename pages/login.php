5fc7b5fa5205f2971264d2e8052df464c43aef23
kornflakesguy <fer.aravalo1997@gmail.com>
2017-12-18 10:58:26 -0600

    MenuMobil

--- pages/login.php
+++ pages/login.php
@@ -1,7 +1,7 @@
 <?php
 echo '
 <div class="login">
-    <form method="post" action="pages/chekLogin.php">
+    <form method="post" action="pages/cuenta.php">
     	<input type="text" style="font-size:25px;" name="usuario" id="usuario" placeholder="Usuario" required="required" />
         <input type="password" style="font-size:25px;" name="pass" id="pass" placeholder="Contraseña" required="required" />
         <button type="submit" class="btn btn-primary btn-block btn-large">Entrar.</button>
