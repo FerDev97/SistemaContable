18bb250e23ac1835c9a3d3706c0bd87a56704917
kornflakesguy <fer.aravalo1997@gmail.com>
2017-12-18 11:10:33 -0600

    Arreglando

--- pages/login.php
+++ pages/login.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 echo '
 <div class="login">
@@ -9,15 +8,3 @@ echo '
     </form>
 </div>'
  ?>
-=======
-<?php
-echo '
-<div class="login">
-    <form method="post" action="pages/cuenta.php">
-    	<input type="text" style="font-size:25px;" name="usuario" id="usuario" placeholder="Usuario" required="required" />
-        <input type="password" style="font-size:25px;" name="pass" id="pass" placeholder="Contraseña" required="required" />
-        <button type="submit" class="btn btn-primary btn-block btn-large">Entrar.</button>
-    </form>
-</div>'
- ?>
->>>>>>> 22769d25a60b72460fce669d3eb62b44819a0ed9
