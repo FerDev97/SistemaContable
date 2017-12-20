29fc5f24ef5bfff2ca1d064763c6b6c50840bce0
kornflakesguy <fer.aravalo1997@gmail.com>
2017-12-20 11:24:37 -0600

    Editar En partida

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ else  {// code for IE6, IE5
 xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
 }xmlhttp.onreadystatechange=function(){if (xmlhttp.readyState==4 && xmlhttp.status==200){document.getElementById("logindiv").innerHTML=xmlhttp.responseText;}}
 
-xmlhttp.open("GET","pages/login.php",true);
+xmlhttp.open("GET","pages/ajaxUser.php",true);
 xmlhttp.send();
 }
 		</script>
--- pages/chekLogin.php
+++ pages/chekLogin.php
@@ -1,5 +1,3 @@
-
-
 <?php
 session_start();
 include "../config/conexion.php";
@@ -7,15 +5,31 @@ if (isset($_POST['login'])) {
   $usuario=$_POST['usuario'];
   $pass=$_POST['pass'];
   $result = $conexion->query("select * from usuarios where usuario=" .$usuario);
-  if(($result->num_rows)>0){
-    $row=mysql_fetch_array($result);
-    $_SESSION['usuario']=$row['usuario'];
-    echo 'Bienvenido a Pacholi 2018',$_SESSION['usuario'],'<p>';
-    echo '<script>window.location="main.php";<script>';
+  if($result){
+    while ($fila = $result->fetch_object()) {
+      echo '<script>prueba();<script>';
+    }
+    //$row=mysql_fetch_array($result);
+  //  $_SESSION['usuario']=$row['usuario'];
+    //echo 'Bienvenido a Pacholi 2018',$_SESSION['usuario'],'<p>';
   }else{
     echo '<script>alert("usuario o caontraseña incorrectos");<script>';
     echo '<script>window.location="../index.php";script>';
   }
 }
-
  ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <script type="text/javascript">
+      function prueba()
+      {
+        alert("funciona validacion d eusaurio");
+      }
+    </script>
+  </head>
+  <body>
+
+  </body>
+</html>
