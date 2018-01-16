185eb1c8a8b6ad8416cbd34b81333a423d54eb1e
kornflakesguy <fer.aravalo1997@gmail.com>
2018-01-16 14:04:46 -0600

    Estado.

--- pages/estado.php
+++ pages/estado.php
@@ -18,76 +18,80 @@ if($result)
   }
 }
 //saldo de ventas
-$resultventa= $conexion->query("select l.debe as debe,l.haber as haber from ldiario as l where l.idcatalogo=11");
+$resultventa= $conexion->query("select c.nombrecuenta as nombre, c.codigocuenta as codigo, SUBSTRING(c.codigocuenta,'1','2') as codigocorto, p.idpartida as npartida, p.concepto as concepto, p.fecha as fecha, l.debe as debe, l.haber as haber FROM catalogo as c,partida as p, ldiario as l where SUBSTRING(c.codigocuenta,1,'2')='51' and p.idpartida=l.idpartida and l.idcatalogo=c.idcatalogo and p.idanio='".$anioActivo."' ORDER BY p.idpartida ASC");
 if ($resultventa) {
     while ($fila = $resultventa->fetch_object()) {
-      $saldoV=$saldoV-($fila->debe)+($fila->haber);
+      if ($fila->saldo=="DEUDOR") {
+        $saldoV=$saldoV+($fila->debe)-($fila->haber);
+      }else {
+        $saldoV=$saldoV-($fila->debe)+($fila->haber);
+      }
       }
   }
   // saldo de reb y dev sobre ventas
-$resultRebDevVet= $conexion->query("select l.debe as debe,l.haber as haber from ldiario as l where l.idcatalogo=12");
+$resultRebDevVet= $conexion->query("select c.nombrecuenta as nombre, c.codigocuenta as codigo, SUBSTRING(c.codigocuenta,'1','3') as codigocorto, p.idpartida as npartida, p.concepto as concepto, p.fecha as fecha, l.debe as debe, l.haber as haber FROM catalogo as c,partida as p, ldiario as l where SUBSTRING(c.codigocuenta,1,'3')='411' and p.idpartida=l.idpartida and l.idcatalogo=c.idcatalogo and p.idanio='".$anioActivo."' ORDER BY p.idpartida ASC");
 if ($resultRebDevVet) {
     while ($fila = $resultRebDevVet->fetch_object()) {
       $saldoRDV=$saldoRDV+($fila->debe)-($fila->haber);
       }
 }
 //Saldo de Compras
-$resultCompras= $conexion->query("select l.debe as debe,l.haber as haber from ldiario as l where l.idcatalogo=14");
+$resultCompras= $conexion->query("select c.nombrecuenta as nombre, c.codigocuenta as codigo, SUBSTRING(c.codigocuenta,'1','2') as codigocorto, p.idpartida as npartida, p.concepto as concepto, p.fecha as fecha, l.debe as debe, l.haber as haber FROM catalogo as c,partida as p, ldiario as l where SUBSTRING(c.codigocuenta,1,'2')='43' and p.idpartida=l.idpartida and l.idcatalogo=c.idcatalogo and p.idanio='".$anioActivo."' ORDER BY p.idpartida ASC");
 if ($resultCompras) {
     while ($fila = $resultCompras->fetch_object()) {
       $saldoComp=$saldoComp+($fila->debe)-($fila->haber);
       }
 }
 //Saldo de Gastos sobre Compras
-$resultGastoComp= $conexion->query("select l.debe as debe,l.haber as haber from ldiario as l where l.idcatalogo=15");
+$resultGastoComp= $conexion->query("select c.nombrecuenta as nombre, c.codigocuenta as codigo, SUBSTRING(c.codigocuenta,'1','2') as codigocorto, p.idpartida as npartida, p.concepto as concepto, p.fecha as fecha, l.debe as debe, l.haber as haber FROM catalogo as c,partida as p, ldiario as l where SUBSTRING(c.codigocuenta,1,'2')='44' and p.idpartida=l.idpartida and l.idcatalogo=c.idcatalogo and p.idanio='".$anioActivo."' ORDER BY p.idpartida ASC");
 if ($resultGastoComp) {
     while ($fila = $resultGastoComp->fetch_object()) {
       $saldoGasComp=$saldoGasComp+($fila->debe)-($fila->haber);
       }
 }
 //Saldo de Reb y dev sobre Compras
-$resultRebDevComp= $conexion->query("select l.debe as debe,l.haber as haber from ldiario as l where l.idcatalogo=16");
+$resultRebDevComp= $conexion->query("select c.nombrecuenta as nombre, c.codigocuenta as codigo, SUBSTRING(c.codigocuenta,'1','2') as codigocorto, p.idpartida as npartida, p.concepto as concepto, p.fecha as fecha, l.debe as debe, l.haber as haber FROM catalogo as c,partida as p, ldiario as l where SUBSTRING(c.codigocuenta,1,'2')='53' and p.idpartida=l.idpartida and l.idcatalogo=c.idcatalogo and p.idanio='".$anioActivo."' ORDER BY p.idpartida ASC");
 if ($resultRebDevComp) {
     while ($fila = $resultRebDevComp->fetch_object()) {
       $saldoRDC=$saldoRDC-($fila->debe)+($fila->haber);
       }
 }
 //Saldo Gastos de admon
-$resuktGasAdmon= $conexion->query("select l.debe as debe,l.haber as haber from ldiario as l where l.idcatalogo=17");
+$resuktGasAdmon= $conexion->query("select c.nombrecuenta as nombre, c.codigocuenta as codigo, SUBSTRING(c.codigocuenta,'1','3') as codigocorto, p.idpartida as npartida, p.concepto as concepto, p.fecha as fecha, l.debe as debe, l.haber as haber FROM catalogo as c,partida as p, ldiario as l where SUBSTRING(c.codigocuenta,1,'3')='415' and p.idpartida=l.idpartida and l.idcatalogo=c.idcatalogo and p.idanio='".$anioActivo."' ORDER BY p.idpartida ASC");
 if ($resuktGasAdmon) {
     while ($fila = $resuktGasAdmon->fetch_object()) {
       $saldoGA=$saldoGA+($fila->debe)-($fila->haber);
       }
 }
 //Saldo Gastos de ventas
-$resuktGasVen= $conexion->query("select l.debe as debe,l.haber as haber from ldiario as l where l.idcatalogo=18");
+$resuktGasVen= $conexion->query("select c.nombrecuenta as nombre, c.codigocuenta as codigo, SUBSTRING(c.codigocuenta,'1','3') as codigocorto, p.idpartida as npartida, p.concepto as concepto, p.fecha as fecha, l.debe as debe, l.haber as haber FROM catalogo as c,partida as p, ldiario as l where SUBSTRING(c.codigocuenta,1,'3')='416' and p.idpartida=l.idpartida and l.idcatalogo=c.idcatalogo and p.idanio='".$anioActivo."' ORDER BY p.idpartida ASC");
 if ($resuktGasVen) {
     while ($fila = $resuktGasVen->fetch_object()) {
       $saldoGV=$saldoGV+($fila->debe)-($fila->haber);
       }
 }
 //Saldo Gastos de Finan
-$resuktGasFina= $conexion->query("select l.debe as debe,l.haber as haber from ldiario as l where l.idcatalogo=19");
+$resuktGasFina= $conexion->query("select c.nombrecuenta as nombre, c.codigocuenta as codigo, SUBSTRING(c.codigocuenta,'1','3') as codigocorto, p.idpartida as npartida, p.concepto as concepto, p.fecha as fecha, l.debe as debe, l.haber as haber FROM catalogo as c,partida as p, ldiario as l where SUBSTRING(c.codigocuenta,1,'3')='417' and p.idpartida=l.idpartida and l.idcatalogo=c.idcatalogo and p.idanio='".$anioActivo."' ORDER BY p.idpartida ASC");
 if ($resuktGasFina) {
     while ($fila = $resuktGasFina->fetch_object()) {
       $saldoGF=$saldoGF+($fila->debe)-($fila->haber);
       }
 }
 //Saldo Otros Gastos
-$resulOG= $conexion->query("select l.debe as debe,l.haber as haber from ldiario as l where l.idcatalogo=20");
+$resulOG= $conexion->query("select c.nombrecuenta as nombre, c.codigocuenta as codigo, SUBSTRING(c.codigocuenta,'1','3') as codigocorto, p.idpartida as npartida, p.concepto as concepto, p.fecha as fecha, l.debe as debe, l.haber as haber FROM catalogo as c,partida as p, ldiario as l where SUBSTRING(c.codigocuenta,1,'3')='423' and p.idpartida=l.idpartida and l.idcatalogo=c.idcatalogo and p.idanio='".$anioActivo."' ORDER BY p.idpartida ASC");
 if ($resulOG) {
     while ($fila = $resulOG->fetch_object()) {
       $saldoOG=$saldoOG+($fila->debe)-($fila->haber);
       }
 }
 //Saldo Otros ingresos
-$resulOI= $conexion->query("select l.debe as debe,l.haber as haber from ldiario as l where l.idcatalogo=21");
+$resulOI= $conexion->query("select c.nombrecuenta as nombre, c.codigocuenta as codigo, SUBSTRING(c.codigocuenta,'1','3') as codigocorto, p.idpartida as npartida, p.concepto as concepto, p.fecha as fecha, l.debe as debe, l.haber as haber FROM catalogo as c,partida as p, ldiario as l where SUBSTRING(c.codigocuenta,1,'3')='521' and p.idpartida=l.idpartida and l.idcatalogo=c.idcatalogo and p.idanio='".$anioActivo."' ORDER BY p.idpartida ASC");
 if ($resulOI) {
     while ($fila = $resulOI->fetch_object()) {
       $saldoOI=$saldoOI-($fila->debe)+($fila->haber);
       }
 }
-$resulII= $conexion->query("select l.debe as debe,l.haber as haber from ldiario as l where l.idcatalogo=22");
+$resulII= $conexion->query("select c.nombrecuenta as nombre, c.codigocuenta as codigo, SUBSTRING(c.codigocuenta,'1','3') as codigocorto, p.idpartida as npartida, p.concepto as concepto, p.fecha as fecha, l.debe as debe, l.haber as haber FROM catalogo as c,partida as p, ldiario as l where SUBSTRING(c.codigocuenta,1,'3')='118' and p.idpartida=l.idpartida and l.idcatalogo=c.idcatalogo and p.idanio='".$anioActivo."' ORDER BY p.idpartida ASC");
 if ($resulII) {
     while ($fila = $resulII->fetch_object()) {
       $saldoII=$saldoII+($fila->debe)-($fila->haber);
